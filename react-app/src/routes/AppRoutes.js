eb85fcdcc3fb35b190a7993fdbce673d15e20f71
Cassandra Aquino <cassandra.aquino@berkeley.edu>
2025-04-29 19:09:58 -0700

    created user profile, login, signup page files; updated routing

--- react-app/src/routes/AppRoutes.js
+++ react-app/src/routes/AppRoutes.js
@@ -6,6 +6,10 @@ import LiveUpdatesPage from '../pages/LiveUpdatesPage';
 import LegalPage from '../pages/LegalPage';
 import CommunityPage from '../pages/CommunityPage';
 import AboutPage from '../pages/AboutPage';
+import UserProfilePage from '../pages/UserProfilePage';
+import LoginPage from '../pages/LoginPage';
+import SignupPage from '../pages/SignupPage';
+
 
 // page linking
 function AppRoutes() { 
@@ -19,6 +23,9 @@ function AppRoutes() {
                 <Route path='/legal' element= {<LegalPage/>}/>
                 <Route path='/community' element= {<CommunityPage/>}/>
                 <Route path='/about' element= {<AboutPage/>}/>
+                <Route path='/profile' element= {<UserProfilePage/>}/>
+                <Route path='/login' element= {<LoginPage/>}/>
+                <Route path='/signup' element= {<SignupPage/>}/>
             </Routes>
         </Router> 
     );
