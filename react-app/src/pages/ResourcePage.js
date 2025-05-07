955e258a2268db43f7ec468b3e2cf9d339d7a6e2
Aishik Bhattacharyya <aishikb@berkeley.edu>
2025-05-06 18:13:41 -0700

    added spinner for search

--- react-app/src/pages/ResourcePage.js
+++ react-app/src/pages/ResourcePage.js
@@ -41,6 +41,7 @@ function ResourcePage() {
 
     const [searchInput, setSearchInput] = useState('');
     const [nogginFilerRes, setNogginFilterRes] = useState(false);
+    const [searchSpinner, setSearchSpinner] = useState(false);
 
     const [activeFilters, setActiveFilters] = useState({
         'resource-type': 'All',
@@ -180,6 +181,7 @@ function ResourcePage() {
     });
 
     const doNogginSearch = async() => {
+        setSearchSpinner(true);
         const allResourceInfo = resources.map((r, i) => `${i}. Title: ${r.title}, Posted By: ${r.postedBy}, Description: ${r.description}, Address: ${r.address}, Location: ${r.location}, Hours: ${r.hours}, Contact: ${r.contact}, Type: ${r.type}, Status: ${r.status}, Time Posted: ${r.timePosted}`).join('\n');
 
         const response = await fetch(
@@ -202,6 +204,7 @@ function ResourcePage() {
 
             const nogginResultsBools = indices.map(i => resources[i]).filter(Boolean);
             setNogginFilterRes(nogginResultsBools);
+            setSearchSpinner(false);
     };
 
     const filterLogic = (resources) => {
@@ -376,8 +379,9 @@ function ResourcePage() {
                     <div className="search-bar">
                         {/* TODO: Implement search functionality */}
                         <input type="text" value={searchInput} placeholder="Search resources..." onChange={(x) => setSearchInput(x.target.value)}/>
-                        <button className="search-btn" onClick={doNogginSearch}>Search</button>
+                        <button className="search-btn" onClick={doNogginSearch} disabled={searchSpinner}>{searchSpinner ? "Searching..." : "Search"}</button>
                         <button className='clear-filters-btn' onClick={clearFiltersFunc}> Clear Filters </button>
+                        {searchSpinner && <div className='spinner'></div>}
                     </div>
                 </div>
 
